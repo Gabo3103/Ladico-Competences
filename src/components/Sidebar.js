1ec532ac627abb8be02d9f497aa9d26e71d29090
Mabrixdee <143139814+Mabrixdee@users.noreply.github.com>
2025-05-07 20:38:58 -0400

    Implementa navegacion a sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,15 @@
 
 import React from 'react';
 
-function Sidebar() {
+function Sidebar({ onNavigate }) {
   return (
     <aside className="sidebar">
       <h2>LADICO</h2>
       <nav>
         <ul>
-          <li className="active">Competencias</li>
-          <li>Mi progreso</li>
-          <li>Explorar</li>
+          <li onClick={() => onNavigate('competencias')}>Competencias</li>
+          <li onClick={() => onNavigate('progreso')}>Mi progreso</li>
+          <li onClick={() => onNavigate('explorar')}>Explorar</li>
         </ul>
       </nav>
     </aside>
